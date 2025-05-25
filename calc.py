80e049ba449218114c214661d5386757d2997c83
Brando <donatobrando02@gmail.com>
2025-05-25 23:26:17 +0800

    added the subtract function

--- calc.py
+++ calc.py
@@ -1,8 +1,11 @@
 def add(a, b):
     return a + b
+def subtract(a, b):
+    return a - b
 
 a = 2
 b = 3
+
 if __name__ == "__main__":
     result = add(a, b)
     print(f"The sum of {a} and {b} is {result}.")
